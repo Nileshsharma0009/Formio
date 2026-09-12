d626d86def217bc359723a693bc8a3016874b045
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-09-12 22:53:41 +0530

    cors and basic backend frontend setup is done

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import {logger} from "./logs/logger.js"
 
 import { httpLogger } from "./logs/logger.js";
 
@@ -17,6 +18,36 @@ app.use(
     },
   })
 );
+
+
+const url = process.env.FRONTEND_URL;
+const interval = Number(process.env.RELOAD_INTERVAL) || 30000;
+
+
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      logger.info("website reloded");
+    })
+    .catch((error) => {
+      // console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://formio-eta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use(cookieParser());
 
