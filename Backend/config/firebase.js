f33b9c4cbe37abf97ee92a4d37772b1d1febd217
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-09-13 19:28:28 +0530

    fixed firebasea apikey

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,9 +1,25 @@
-import {
-  initializeApp,
-  cert,
-} from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
+import dotenv from "dotenv";
 
-import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
+dotenv.config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:
+    process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 const app = initializeApp({
   credential: cert(serviceAccount),
@@ -12,3 +28,5 @@ const app = initializeApp({
 console.log("🔥 Firebase Admin initialized");
 
 export default app;
+
+
